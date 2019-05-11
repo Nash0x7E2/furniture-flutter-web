d39221c4ae5d847a228a205137de4656131c5f4f
Nash Ramdial <mail@neevash.dev>
2019-05-11 10:34:24 -0400

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,106 @@
 import 'package:flutter_web/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Furniture(),
+    ),
+  );
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Furniture extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
-            ),
-          ],
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(
+          "Furniture",
+          style: TextStyle(color: Colors.black),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          Icon(
+            Icons.search,
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Image.network(
+              "https://ak1.ostkcdn.com/img/mxc/02142019_Furniture_AMOD_DESKTOP.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+          const SizedBox(width: 20.0),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Carica store new product",
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16.0),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  "Outdoor Furniture",
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 50.0),
+                ),
+                const SizedBox(height: 14.0),
+                Text(
+                  "Recliner lad, eu mollis diam, vitae gravida mauris. Cras mollis malesuada sem vitae venenatis. Morbi at erat eget nulla placerat egestas ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 24.0,
+                  ),
+                  textAlign: TextAlign.justify,
+                  overflow: TextOverflow.fade,
+                ),
+                const SizedBox(height: 20.0),
+                FlatButton(
+                  onPressed: () {},
+                  color: Color(0xFFCFE8E4),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text("Read More"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(48.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.search),
+                  Icon(Icons.search),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(width: 20.0),
+        ],
+      ),
     );
   }
 }
