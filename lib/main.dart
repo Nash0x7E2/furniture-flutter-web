1de747fc1317863cbe440c0733621cacf0d6ecc3
Nash Ramdial <mail@neevash.dev>
2019-05-13 15:00:52 -0400

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_web/material.dart';
+import 'package:furniture/utils/responsive_layout.dart';
 import 'package:furniture/widgets/button_row.dart';
 
 void main() {
@@ -22,7 +23,7 @@ class Furniture extends StatelessWidget {
         title: Text(
           "Furniture",
           style:
-              TextStyle(color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+              TextStyle(color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.w800, fontStyle: FontStyle.italic),
         ),
         leading: Image.asset("icon/nav-icon.png"),
         actions: <Widget>[
@@ -35,25 +36,32 @@ class Furniture extends StatelessWidget {
           )
         ],
       ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: SizedBox(
-                height: 50.0,
-                child: ButtonRow(),
+      body: ResponsiveLayout(
+        largeChild: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(48.0),
+                child: SizedBox(
+                  height: 50.0,
+                  child: ButtonRow(),
+                ),
               ),
             ),
-          ),
-          _buildBody(),
-        ],
+            Body(),
+          ],
+        ),
+        smallChild: SmallBody(),
       ),
     );
   }
+}
+class Body extends StatelessWidget {
+  const Body({Key key}) : super(key: key);
 
-  Row _buildBody() {
+  @override
+  Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -110,46 +118,67 @@ class Furniture extends StatelessWidget {
   }
 }
 
-class ButtonRow extends StatelessWidget {
-  const ButtonRow({Key key}) : super(key: key);
+
+class SmallBody extends StatelessWidget {
+  const SmallBody({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
             child: Image.asset(
-              "social/facebook.png",
-              height: 15.0,
+              "background-image.jpg",
+              fit: BoxFit.fill,
             ),
           ),
-        ),
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
-            child: Image.asset(
-              "social/twitter.png",
-              height: 15.0,
+          const SizedBox(width: 60.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 40.0),
+                Text(
+                  "Carica store new product",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16.0, fontFamily: "Open Sans"),
+                ),
+                Text(
+                  "Interior Design",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w600, fontSize: 40.0, fontFamily: 'Merriweather'),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  "Recliner lad, eu mollis diam, vitae gravida mauris. Cras mollis malesuada sem vitae venenatis. Morbi at erat eget nulla placerat egestas ",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 18.0, fontFamily: "Open Sans"),
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(height: 20.0),
+                FlatButton(
+                  onPressed: () {},
+                  color: Color(0xFFCFE8E4),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text("Read More", style: TextStyle(fontFamily: "Open Sans")),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
-            child: Image.asset(
-              "social/instagram.png",
-              height: 15.0,
-            ),
+          ButtonRow(
+            size: MainAxisSize.max,
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
