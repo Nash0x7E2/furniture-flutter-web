493b355a8a9aaf4ea1168c10f3b68a2d320e7578
Nash Ramdial <mail@neevash.dev>
2019-05-13 16:53:26 -0400

    added a size to each button

--- lib/widgets/button_row.dart
+++ lib/widgets/button_row.dart
@@ -8,34 +8,44 @@ class ButtonRow extends StatelessWidget {
     return Row(
       mainAxisSize: size,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
-            child: Image.asset(
-              "social/facebook.png",
-              height: 15.0,
+        SizedBox(
+          height: 50.0,
+          child: FlatButton(
+            onPressed: () {},
+            color: Color(0XFFFBEFD9),
+            child: Center(
+              child: Image.asset(
+                "social/facebook.png",
+                height: 15.0,
+              ),
             ),
           ),
         ),
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
-            child: Image.asset(
-              "social/twitter.png",
-              height: 15.0,
+        SizedBox(
+          height: 50,
+          child: FlatButton(
+            onPressed: () {},
+            color: Color(0XFFFBEFD9),
+            child: Center(
+              child: Image.asset(
+                "social/twitter.png",
+                height: 15.0,
+              ),
             ),
           ),
         ),
-        FlatButton(
-          onPressed: () {},
-          color: Color(0XFFFBEFD9),
-          child: Center(
-            child: Image.asset(
-              "social/instagram.png",
-              height: 15.0,
+        SizedBox(
+          height: 50,
+          child: FlatButton(
+            onPressed: () {},
+            color: Color(0XFFFBEFD9),
+            child: Center(
+              child: Image.asset(
+                "social/instagram.png",
+                height: 15.0,
+              ),
             ),
           ),
         ),
