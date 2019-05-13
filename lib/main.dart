df8ce53c6521db43767fc8eef12fcf61cbb2fbe9
Nash Ramdial <mail@neevash.dev>
2019-05-13 17:30:58 -0400

    added scrolling for small screens

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class Furniture extends StatelessWidget {
     );
   }
 }
+
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
 
@@ -118,67 +119,99 @@ class Body extends StatelessWidget {
   }
 }
 
-
 class SmallBody extends StatelessWidget {
   const SmallBody({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Image.asset(
-              "background-image.jpg",
-              fit: BoxFit.fill,
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: constraints.copyWith(
+              minHeight: constraints.maxHeight,
+              maxHeight: double.infinity,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Image.asset(
+                      "background-image.jpg",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  const SizedBox(height: 60.0),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const SizedBox(height: 40.0),
+                          Text(
+                            "Carica store new product",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.0,
+                                fontFamily: "Open Sans"),
+                          ),
+                          Text(
+                            "Interior Design",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 35.0,
+                                fontFamily: 'Merriweather'),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Text(
+                            "Recliner lad, eu mollis diam, vitae gravida mauris. Cras mollis malesuada sem vitae venenatis. Morbi at erat eget nulla placerat egestas ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20.0,
+                                fontFamily: "Open Sans"),
+                            textAlign: TextAlign.justify,
+                          ),
+                          const SizedBox(height: 20.0),
+                          FlatButton(
+                            onPressed: () {},
+                            color: Color(0xFFCFE8E4),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("Read More", style: TextStyle(fontFamily: "Open Sans")),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        width: double.infinity,
+                        color: Color(0XFFFBEFD9),
+                        padding: EdgeInsets.all(12.0),
+                        child: ButtonRow(
+                          size: MainAxisSize.max,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          const SizedBox(width: 60.0),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 40.0),
-                Text(
-                  "Carica store new product",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16.0, fontFamily: "Open Sans"),
-                ),
-                Text(
-                  "Interior Design",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w600, fontSize: 40.0, fontFamily: 'Merriweather'),
-                ),
-                const SizedBox(height: 10.0),
-                Text(
-                  "Recliner lad, eu mollis diam, vitae gravida mauris. Cras mollis malesuada sem vitae venenatis. Morbi at erat eget nulla placerat egestas ",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 18.0, fontFamily: "Open Sans"),
-                  textAlign: TextAlign.justify,
-                ),
-                const SizedBox(height: 20.0),
-                FlatButton(
-                  onPressed: () {},
-                  color: Color(0xFFCFE8E4),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text("Read More", style: TextStyle(fontFamily: "Open Sans")),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          ButtonRow(
-            size: MainAxisSize.max,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
